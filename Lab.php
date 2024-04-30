b42daba5b5e382ac8bbb1fe529b123fc0984c764
steven-halla <steven.r.halla@gmail.com>
2024-04-30 15:58:30 -0700

    first

---
+++ Lab.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello World</title>
+</head>
+
+<body>
+<?php
+//echo "Hello, <b>World!</b><br>";
+//echo "<b>Hello, World!</b>";
+//echo "This is standard HTML.<br><br></center> ";
+//echo "    Steven Halla, Hello PHP.";
+//echo "This was generated using PHP!<br><br></center>";
+//echo "Steven Halla!<br><br></center>";
+//echo "CIS 242!<br></center>";
+
+echo "This is a line of text.<br>";
+echo "This is another line of text.<br><br>";
+//echo "Now I'm done.<br><br>";
+echo "This line will not be executed.<br><br>";
+
+
+?>
+</body>
+</html>
---
+++ concat.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Concatenation Example</title>
+</head>
+<body>
+<?php
+$part1 = "The book ";
+$part2 = "<em>The Girls' Guide to Hunting and Fishing</em> ";
+$part3 = "was written by Melissa Bank,";
+$part4 = " Steven Halla.";
+
+$fullSentence = $part1 . $part2 . $part3 . $part4;
+
+echo $fullSentence;
+?>
+</body>
+</html>
